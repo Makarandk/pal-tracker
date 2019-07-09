e3d9b8df5eda95e2a95fd0b8ad5bafa5cf233989
Makarandk <Makarand_Narendra_ks@dell.com>
2019-07-10 01:09:56 +0530

    WelcomeChaneg.csas

--- src/PalTracker/WelcomeMessage.cs
+++ src/PalTracker/WelcomeMessage.cs
@@ -5,7 +5,7 @@ namespace PalTracker
         public string Message {get;}
         public WelcomeMessage(string message)
         {
-            Message=message+"hi";
+            Message=message+"hi hi";
         }
     }
 }
